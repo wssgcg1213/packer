fc2d9cdb6970f2ba68d704441078ab83423487b9
wssgcg1213 <i@zeroling.com>
2016-12-01 23:02:26 +0800

    feat: support browserSync

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,31 @@
 'use strict';
 const yargs = require('yargs')
 const path = require('path')
+const fs = require('fs')
 const cwd = process.cwd()
 
 const options = {
   cwd: cwd
 }
 
-console.log(yargs.argv)
+// console.log(yargs.argv)
+try {
+  const srcFiles = fs.readdirSync(path.join(cwd, 'src'))
+  const entry = srcFiles.filter((filename) => /^index.jsx?$/.test(filename))
+                        .map((filename) => path.join(cwd, 'src', filename))
+  if (entry.length === 0) {
+    throw new Error('Entry Not Found')
+  }
+  options.entry = entry
+  entry.forEach((file) => console.log(`Detected entry: ${entry}`))
+  console.log('Start compiling...')
+} catch(err) {
+  console.log('Can not find entry file. Please check src/index.jsx exists.')
+}
 
 require('../lib')(options)
   .then(() => require('../server')(options))
+  .catch((err) => {
+    console.log('Error while cli')
+    console.error(err.stack)
+  })
--- server/index.js
+++ server/index.js
@@ -1,31 +1,52 @@
 'use strict'
-const Koa = require('koa')
-const koaStatic = require('koa-static');
+
+const browserSync = require("browser-sync")
+const serveIndex = require('serve-index')
 const path = require('path')
 const fs = require('fs')
 const blacklist = {
   'node_modules': true
 }
+const bs = browserSync.create()
 
-module.exports = (options) => {
-  const app = new Koa()
+module.exports = exports = (options) => {
   const cwd = options.cwd
   const port = options.port || 3333
-  
-  app.use(koaStatic(path.resolve(cwd), { index: '_index.html' }))
+  const index = serveIndex(cwd, { icons: true, view: 'details' })
 
-  app.use(function *() {
-    const requestPath = path.join(cwd, this.path)
-    const files = fs.readdirSync(requestPath).filter((filename) => filename[0] !== '.' && !blacklist[filename])
-    this.body = files
+  bs.init({ 
+    server: cwd,
+    port: port,
+    middleware: [
+      (req, res, next) => {
+        if (/favicon\.ico$/i.test(req.url)) {
+          return res.end('')
+        }
+        try {
+          const stat = fs.statSync(path.join(cwd, req.url))
+          if (stat.isDirectory()) {
+            index(req, res)
+          } else {
+            next()
+          }
+        } catch(err) {
+          next()
+        }
+      },
+
+      (req, res, next) => {
+        const requestPath = path.join(cwd, req.url)
+        try {
+          const files = fs.readdirSync(requestPath).filter((filename) => filename[0] !== '.' && !blacklist[filename])
+          res.end(files.toString())
+        } catch(err) {
+          next()
+        }
+      }
+    ]
   })
-  app.listen(port, (err) => {
-    if (err) {
-      console.log('Dev Server error with detail')
-      console.log(err.stack)
-    } else {
-      console.log(`Dev Server at http://localhost:${port}`)
-    }
-  })
-  return app
+
+  return bs
 }
+
+exports.browserSync = bs
